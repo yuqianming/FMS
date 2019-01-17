5c564a3e13764132b069594a756003e979153b3d
yuqianming <17695556392@163.com>
2019-01-17 14:32:29 +0800

    wqq

---
+++ testIdea/untitled/src/com/xinzhi/test1/demo1.java
@@ -0,0 +1,10 @@
+package com.xinzhi.test1;
+
+public class demo1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(1);
+            System.out.println(2);
+        }
+    }
+}
